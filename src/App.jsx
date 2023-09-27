98ffb0a2fb3ec850cf3004684e601825e4e5c525
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2023-09-27 11:58:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter } from "react-router-dom";
+import AboutMe from '../src/components/About'
+import ContactMe from '../src/components/Contact'
+import Experience from '../src/components/Experience'
+import Feedbacks from '../src/components/Feedbacks'
+import Hero from '../src/components/Hero'
+import Loader from '../src/components/Loader'
+import Navbar from '../src/components/Navbar'
+import Tech from '../src/components/Tech'
+import Works from '../src/components/Works'
+import StarCanvas from '../src/components/canvas/Stars'
+
+const App = () => {
+ 
+
+  return (
+    <>
+     
+     <BrowserRouter>
+          <div className="relative z-0 bg-primary">
+              <div className="bg-hero-pattern bg-cover bg-no-repeat bg center">
+                  <Navbar/>
+                  
+              </div>
+              <AboutMe/>
+              <Experience/>
+              <Tech/>
+              <Works/>
+              <Feedbacks/>
+              <div className="relative z-0">
+                <ContactMe/>
+                <StarCanvas/>
+              </div>
+          </div>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {motion} from 'framer-motion'
+import {style} from '../style'
+
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen mx-auto'>Hero</section>
+  )
+}
+
+export default Hero
